6ac05bfd699ef2a606f81a8c5b0e59d117e8be13
levworkshop <noreply@githubusers.com>
2022-12-29 18:24:56 +0200

    template with include

--- index.php
+++ index.php
@@ -1,55 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include_once "./inc/header.php";
+include_once "./inc/navbar.php";
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="./assets/css/catalog.css">
-    <title>The Catalog</title>
-</head>
+<main>
+    <div class="text-center">
+        <h2>Sport Superstar</h2>
+        <h5>Everything that you need to stay active</h5>
+    </div>
 
-<body class="bg-dark text-white">
-    <?php
+    <h5>Trending Products</h5>
 
-    ?>
-
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">The Catalog</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">About</a>
-                        </li>
-                    </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-light" type="submit">Search</button>
-                    </form>
-                </div>
-            </div>
-        </nav>
-    </header>
-
-    <main>
-        <div class="text-center">
-            <h2>Sport Superstar</h2>
-            <h5>Everything that you need to stay active</h5>
-        </div>
-
-        <h5>Trending Products</h5>
-
-        <h5>Our Special Offers</h5>
-    </main>
+    <h5>Our Special Offers</h5>
+</main>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php
+include_once "./inc/footer.php";
