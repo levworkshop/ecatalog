2b150160964959e8983850d44a0f907f08128c5d
levworkshop <noreply@githubusers.com>
2023-01-09 18:26:32 +0200

    products from database

--- index.php
+++ index.php
@@ -16,29 +16,33 @@ include_once "./inc/navbar.php";
 
         <?php
 
-        $products = [
-            [
-                'name' => 'Mike Running Shoes',
-                'description' => 'Very nice product',
-                'price' => 54,
-                'image' => 'https://cdn.pixabay.com/photo/2014/05/18/11/26/shoes-346986__340.jpg',
-                'rating' => 5,
-            ],
-            [
-                'name' => 'Tennis Edge',
-                'description' => 'Description here...',
-                'price' => 38,
-                'image' => 'https://cdn.pixabay.com/photo/2021/06/04/06/54/racket-6308994__340.jpg',
-                'rating' => 4,
-            ],
-            [
-                'name' => 'Weight It',
-                'description' => 'Mmm... expansive!',
-                'price' => 108,
-                'image' => 'https://cdn.pixabay.com/photo/2016/08/31/22/20/weights-1634747__340.jpg',
-                'rating' => 5,
-            ],
-        ];
+        // $products = [
+        //     [
+        //         'name' => 'Mike Running Shoes',
+        //         'description' => 'Very nice product',
+        //         'price' => 54,
+        //         'image' => 'https://cdn.pixabay.com/photo/2014/05/18/11/26/shoes-346986__340.jpg',
+        //         'rating' => 5,
+        //     ],
+        //     [
+        //         'name' => 'Tennis Edge',
+        //         'description' => 'Description here...',
+        //         'price' => 38,
+        //         'image' => 'https://cdn.pixabay.com/photo/2021/06/04/06/54/racket-6308994__340.jpg',
+        //         'rating' => 4,
+        //     ],
+        //     [
+        //         'name' => 'Weight It',
+        //         'description' => 'Mmm... expansive!',
+        //         'price' => 108,
+        //         'image' => 'https://cdn.pixabay.com/photo/2016/08/31/22/20/weights-1634747__340.jpg',
+        //         'rating' => 5,
+        //     ],
+        // ];
+        
+        $db_conn = mysqli_connect('localhost', 'root', 'password', 'catalog');
+        $sql = "SELECT * FROM products";
+        $result = mysqli_query($db_conn, $sql);
 
         $recommended = ['Tennis Edge', 'Weight It'];
 
@@ -47,26 +51,28 @@ include_once "./inc/navbar.php";
             return in_array($name, $recommended) ? "bi-star-fill" : "bi-star";
         };
 
-        // $len = count($products);
-        foreach ($products as $item) {
-            $col = <<<COL
-                <div class="col-sm-12 col-md-4 mb-3">
-                    <div class="card">
-                        <img src="{$item['image']}" class="card-img-top" alt="{$item['name']}">
-                        <div class="card-body text-dark">
-                            <h5 class="card-title">{$item['name']}</h5>
-                            <p class="card-text">{$fn_price($item['price'])}</p> 
-                            <p class="card-text">{$item['description']}</p>
-                            <p class="card-text">rating: {$item['rating']}</p>
-                            <p class="card-text"><i class="{$fn_icon($item['name'])}"></i></p>
-                            <a href="product.php?prod={$item['name']}" class="btn btn-primary">View Product</a>
+        if ($result && mysqli_num_rows($result) > 0) {
+            while ($item = mysqli_fetch_assoc($result)) {
+                $col = <<<COL
+                    <div class="col-sm-12 col-md-4 mb-3">
+                        <div class="card">
+                            <img src="{$item['image']}" class="card-img-top" alt="{$item['name']}">
+                            <div class="card-body text-dark">
+                                <h5 class="card-title">{$item['name']}</h5>
+                                <p class="card-text">{$fn_price($item['price'])}</p> 
+                                <p class="card-text">{$item['description']}</p>
+                                <p class="card-text">rating: {$item['rating']}</p>
+                                <p class="card-text"><i class="{$fn_icon($item['name'])}"></i></p>
+                                <a href="product.php?prod={$item['name']}" class="btn btn-primary">View Product</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            COL;
+                COL;
 
-            echo $col;
+                echo $col;
+            }
         }
+        
         ?>
     </div>
 
